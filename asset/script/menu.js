60513cb4c21a0669c4ca1bc853e3661e7896660d
hara-94 <hikaru.h9894@gmail.com>
2022-08-09 21:00:57 +0900

    add: menu_pc

---
+++ asset/script/menu.js
@@ -0,0 +1,44 @@
+window.onload = () => {
+  const menuSoftdrink = document.getElementById("menu-softdrink");
+  const menuAlcohol = document.getElementById("menu-alcohol");
+  const menuChampagne = document.getElementById("menu-champagne");
+  const menuFood = document.getElementById("menu-food");
+
+  const contentSoftdrink = document.getElementById("content-softdrink");
+  const contentAlcohol = document.getElementById("content-alcohol");
+  const contentChampagne = document.getElementById("content-champagne");
+  // const contentFood = document.getElementById("content-food");
+  
+  const tabs = [menuSoftdrink, menuAlcohol, menuChampagne, menuFood];
+  const contents = [contentSoftdrink, contentAlcohol, contentChampagne];
+
+  function switchTab(target) {
+    tabs.forEach((tab) => { tab.classList.remove("js-selected") });
+    target.classList.add("js-selected");
+  }
+
+  function switchContent(target) {
+    console.log(contents)
+    contents.forEach((content) => { content.classList.remove("show") });
+    console.log(target)
+    target.classList.add("show");
+  }
+  
+  menuSoftdrink.addEventListener("click", (e) => {
+    switchTab(e.target);
+    switchContent(contentSoftdrink);
+  });
+  
+  menuAlcohol.addEventListener("click", (e) => {
+    switchTab(e.target);
+    switchContent(contentAlcohol);
+  });
+  menuChampagne.addEventListener("click", (e) => {
+    switchTab(e.target);
+    switchContent(contentChampagne);
+  });
+  menuFood.addEventListener("click", (e) => {
+    switchTab(e.target);
+    switchContent(contentFood);
+  });
+}
