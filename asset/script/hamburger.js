19bf3ea0e30052bf68a8fee8348c7b659979be5e
hara-94 <hikaru.h9894@gmail.com>
2022-08-15 21:49:13 +0900

    fix: hamburger-animation

--- asset/script/hamburger.js
+++ asset/script/hamburger.js
@@ -2,54 +2,60 @@ export function controlHamburger() {
   const html = document.querySelector("html");
 
   const hamburger = document.getElementById("js-hamburger");
-  const openButton = document.getElementById("js-hamburger-open");
-  const closeButton = document.getElementById("js-hamburger-close");
+  const button = document.getElementById("js-hamburger-button");
 
-  const conceptLink = document.getElementById("js-hamburger-concept");
-  const drinkLink = document.getElementById("js-hamburger-drink");
-  // const gameLink = document.getElementById("js-hamburger-game");
-  const accessLink = document.getElementById("js-hamburger-access");
+  const topBar = document.getElementById("js-hamburger-top-bar");
+  const middleBar = document.getElementById("js-hamburger-middle-bar");
+  const bottomBar = document.getElementById("js-hamburger-bottom-bar");
+
+  const conceptLink = document.getElementById("js-hamburger-concept-link");
+  const drinkLink = document.getElementById("js-hamburger-drink-link");
+  // const gameLink = document.getElementById("js-hamburger-game-link");
+  const accessLink = document.getElementById("js-hamburger-access-link");
 
   const conceptSection = document.getElementById("js-concept-section");
   const accessSection = document.getElementById("js-access-section");
 
-  const items = [
-    conceptLink, 
-    drinkLink, 
-    // gameLink, 
-    accessLink
-  ];
-
   let scrollY = 0;
 
-  openButton?.addEventListener("click", () => {
-    scrollY = window.scrollY;
-    html.classList.add("fixed");
-    hamburger.classList.add("open");
-    hamburger.classList.remove("close");
+  function showHamburger() {
+    hamburger.classList.add("show");
+    hamburger.classList.remove("hide");
+    topBar.classList.add("show");
+    middleBar.classList.add("show");
+    bottomBar.classList.add("show");
+    topBar.classList.remove("hide");
+    middleBar.classList.remove("hide");
+    bottomBar.classList.remove("hide");
+  }
 
-    items.forEach((item, index) => {
-      setTimeout(() => {
-        item.classList.add("show");
-      }, 200 * index);
-    });
-  });
+  function hideHamburger() {
+    hamburger.classList.add("hide");
+    hamburger.classList.remove("show");
+    topBar.classList.add("hide");
+    middleBar.classList.add("hide");
+    bottomBar.classList.add("hide");
+    topBar.classList.remove("show");
+    middleBar.classList.remove("show");
+    bottomBar.classList.remove("show");
+  }
 
-  closeButton?.addEventListener("click", () => {
-    html.classList.remove("fixed");
-    window.scrollTo({ top: scrollY });
-    hamburger.classList.add("close");
-    hamburger.classList.remove("open");
-    items.forEach(item => {
-      item.classList.remove("show");
-    });
+  button?.addEventListener("click", () => {
+    if (hamburger.classList.contains("show")) {
+      html.classList.remove("fixed");
+      window.scrollTo({ top: scrollY });
+      hideHamburger();
+    } else {
+      scrollY = window.scrollY;
+      html.classList.add("fixed");
+      showHamburger();
+    }
   });
 
   conceptLink?.addEventListener("click", (event) => {
     event.preventDefault();
     html.classList.remove("fixed");
-    hamburger.classList.add("close");
-    hamburger.classList.remove("open");
+    hideHamburger();
     window.scrollTo({ top: scrollY });
     if (conceptSection) {
       window.scrollTo({
@@ -63,8 +69,7 @@ export function controlHamburger() {
     event.preventDefault();
     if (accessSection) {
       html.classList.remove("fixed");
-      hamburger.classList.add("close");
-      hamburger.classList.remove("open");
+      hideHamburger();
       window.scrollTo({
         top: window.scrollY + accessSection.getBoundingClientRect().top - 40,
         behavior: "smooth"
